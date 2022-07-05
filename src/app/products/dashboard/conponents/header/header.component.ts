189db42733b46b2af63b2b264673084ce8233e0a
MaxPalan <maxpalanwork@gmail.com>
2022-07-05 14:41:18 +0300

    plan-info component code is in order

--- src/app/products/dashboard/conponents/header/header.component.ts
+++ src/app/products/dashboard/conponents/header/header.component.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 import {Component, EventEmitter, HostListener, Input, OnInit, Output} from '@angular/core';
 
 @Component({
--- src/app/products/dashboard/conponents/plan-info/plan-info.component.ts
+++ src/app/products/dashboard/conponents/plan-info/plan-info.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import { DashboardOverview } from 'src/app/core/models/dashboard-overview.model';
+import {DashboardOverview} from 'src/app/core/models/dashboard-overview.model';
 import {SubscriptionData, User} from "../../../../core/models/user.model";
 
 @Component({
@@ -9,19 +9,41 @@ import {SubscriptionData, User} from "../../../../core/models/user.model";
 })
 export class PlanInfoComponent implements OnInit {
 
+  /**
+   * Current user subscription input data
+   * @type {SubscriptionData | undefined}
+   */
   @Input() subscriptionData!: SubscriptionData | undefined;
 
+  /**
+   * Current user dashboard input data
+   * @type {DashboardOverview | undefined | null}
+   */
   @Input() overviewData!: DashboardOverview | undefined | null;
 
+  /**
+   * Current user input data
+   * @type {User | undefined}
+   */
   @Input() currentUser!: User | undefined;
 
+  /**
+   * Variable for link to extend plan
+   * @type {string}
+   */
   extendPlanLink!: string;
 
+  /**
+   * Constructor for PlanInfoComponent
+   */
   constructor() { }
 
+  /**
+   * Call on component init
+   * @return {void}
+   */
   ngOnInit(): void {
     this.extendPlanLink = this.currentUser?.email.endsWith('@zoogvpn.com') ?
       'https://zoogvpn.com/pricing/' : 'https://app.zoogvpn.com/plans';
   }
-
 }

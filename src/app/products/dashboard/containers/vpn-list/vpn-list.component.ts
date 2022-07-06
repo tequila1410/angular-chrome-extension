1fd249dfcfb227693631e4127027048be5cfd8bc
MaxPalan <maxpalanwork@gmail.com>
2022-07-06 21:27:01 +0300

    vpn-list container code is in order

--- src/app/products/dashboard/containers/vpn-list/vpn-list.component.ts
+++ src/app/products/dashboard/containers/vpn-list/vpn-list.component.ts
@@ -1,15 +1,14 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
-import { Subject} from "rxjs";
+import {Subject} from "rxjs";
 import {ProxyModel} from "../../../../auth/models/proxy.model";
 import {Store} from "@ngrx/store";
 import {AppState} from "../../../../core/store/app.reducer";
-import {bestServerSelect, connecting, setRecentlyUsed, setSelectedServer} from "../../../../core/store/vpn/vpn.actions";
-import {ServerApi} from "../../../../core/api/server.api";
+import {bestServerSelect, setRecentlyUsed, setSelectedServer} from "../../../../core/store/vpn/vpn.actions";
 import {getServerList, isBestServerSelected} from "../../../../core/store/vpn/vpn.selector";
 import {take, takeUntil, tap} from "rxjs/operators";
 import {FormControl} from "@angular/forms";
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-vpn-list',
@@ -26,29 +25,65 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
   ]
 })
 export class VpnListComponent implements OnInit, OnDestroy {
-
+  
+  /**
+   * List of all proxies
+   * @type {ProxyModel[]}
+   */
   proxyData!: ProxyModel[];
-
+  
+  /**
+   * List of filtered proxies
+   * @type {ProxyModel[]}
+   */
   proxyDataFilter!: ProxyModel[];
-
+  
+  /**
+   * List of recently used proxies
+   * @type {ProxyModel[]}
+   */
   proxyDataUsed!: ProxyModel[];
-
-  formControl: FormControl = new FormControl([]);
-
+  
+  /**
+   * Form control for search location input
+   * @type {FormControl}
+   */
+  searchLocation: FormControl = new FormControl([]);
+  
+  /**
+   * Form control for best ping finding checkbox
+   * @type {FormControl}
+   */
   bestPingCheckbox: FormControl = new FormControl();
-
+  
+  /**
+   * Varianle for recognize available feature of proxy
+   * @type {boolean}
+   */
   availableFeature: boolean = false;
-
+  
+  /**
+   * Subject to destroy all subscriptions on component destroy
+   * @type {Subject<void>}
+   */
   destroy$: Subject<void> = new Subject<void>();
 
-  constructor(private router: Router,
-              private store: Store<AppState>) {
+  /**
+   * Constructor for VpnListComponent
+   * @param {Router} router 
+   * @param {Store<AppState>} store 
+   */
+  constructor(private router: Router, private store: Store<AppState>) {
     const proxyDataUsed = localStorage.getItem('recentlyUsed');
     if (proxyDataUsed) {
       this.proxyDataUsed = JSON.parse(proxyDataUsed);
     }
   }
 
+  /**
+   * Call on component init
+   * @return {void}
+   */
   ngOnInit(): void {
     this.store.select(getServerList)
       .pipe(
@@ -66,7 +101,7 @@ export class VpnListComponent implements OnInit, OnDestroy {
         this.bestPingCheckbox.setValue(isBestServerSelected);
       });
 
-    this.formControl.valueChanges.subscribe(res => {
+    this.searchLocation.valueChanges.subscribe(res => {
       this.proxyDataFilter = this.proxyData.filter(proxy => proxy.locationName.toLowerCase().includes(res));
     });
 
@@ -79,11 +114,17 @@ export class VpnListComponent implements OnInit, OnDestroy {
           .reduce((a, b) => (a.ping < b.ping ? a : b))
         :
         this.proxyData.filter(proxy => proxy.host !== 'locked' && proxy.ping > 0)[0];
+        
       this.store.dispatch(setSelectedServer({selectedServer}));
     });
   }
 
-  selectLocation(proxy: ProxyModel) {
+  /**
+   * Set selected proxy for extension & update recently used proxy list
+   * @param {ProxyModel} proxy
+   * @return {void}
+   */
+  selectLocation(proxy: ProxyModel): void {
     if (proxy.host !== 'locked') {
       this.store.dispatch(setRecentlyUsed({recentlyUsedProxy: proxy}));
       this.store.dispatch(setSelectedServer({selectedServer: proxy}));
@@ -92,14 +133,28 @@ export class VpnListComponent implements OnInit, OnDestroy {
     }
   }
 
-  goToDashboard() {
+  /**
+   * Redirect to dashboard page
+   * @return {void}
+   */
+  goToDashboard(): void {
     this.router.navigate(['/dashboard']);
   }
 
-  trackByProxyId(index: number, proxy: ProxyModel) {
+  /**
+   * Used to track only needed proxy
+   * @param {number} index 
+   * @param {ProxyModel} proxy 
+   * @return {string}
+   */
+  trackByProxyId(index: number, proxy: ProxyModel): string {
     return proxy.id;
   }
 
+  /**
+   * Call on component destroy
+   * @return {void}
+   */
   ngOnDestroy(): void {
     this.destroy$.next();
   }

7df0ee4fdaf60a57fc8b0d0881384348a387c7b6
MaxPalan <maxpalanwork@gmail.com>
2022-07-11 21:11:38 +0300

    indexed DB code is in order

--- src/app/core/utils/indexedDB/exclusion-db.service.ts
+++ src/app/core/utils/indexedDB/exclusion-db.service.ts
@@ -7,16 +7,31 @@ import {ExclusionLink} from "../../models/exclusion-link.model";
   providedIn: 'root'
 })
 export class ExclusionDbService {
-
+  
+  /**
+   * Interface of the IndexedDB API provides access to the results of requests to open or delete databases
+   * @type {IDBOpenDBRequest}
+   */
   private openRequest!: IDBOpenDBRequest;
-
+  
+  /**
+   * Interface of the IndexedDB API provides a connection to a database
+   * @type {IDBDatabase}
+   */
   private db!: IDBDatabase;
 
+  /**
+   * Constructor for ExclusionDbService
+   */
   constructor() {
     console.log('IndexedDBService constructor');
   }
 
-  private exclusionsIndexDBInit() {
+  /**
+   * Opens exclusions data base
+   * @return {Observable<unknown>}
+   */
+  private exclusionsIndexedDBInit(): Observable<unknown> {
     return new Observable((subscriber) => {
       this.openRequest = indexedDB.open('exclusionsDB', 1);
       this.openRequest.onupgradeneeded = () => {
@@ -41,7 +56,7 @@ export class ExclusionDbService {
         this.db = this.openRequest.result;
         this.db.onversionchange = () => {
           this.db.close();
-          console.log("База данных устарела, пожалуста, перезагрузите страницу.")
+          console.log("The database is out of date, please reload the page.")
         };
 
         subscriber.next(this.db);
@@ -49,10 +64,15 @@ export class ExclusionDbService {
     })
   }
 
+  /**
+   * Handler for recognize data base section
+   * @param {Function} fn 
+   * @return {Observable<any>}
+   */
   private handler(fn: Function): Observable<any> {
     fn = fn.bind(this);
     if (!this.openRequest?.result) {
-      return this.exclusionsIndexDBInit()
+      return this.exclusionsIndexedDBInit()
         .pipe(exhaustMap(() => {
         return fn();
       }));
@@ -60,14 +80,26 @@ export class ExclusionDbService {
     return fn();
   }
 
+  /**
+   * Get regural mode exclusions
+   * @return {Observable<ExclusionLink[]>}
+   */
   public getRegularLinks(): Observable<ExclusionLink[]> {
     return this.handler(this.getRegularLinksHandler)
   }
 
+  /**
+   * Get selective mode exclusions
+   * @return {Observable<ExclusionLink[]>}
+   */
   public getSelectiveLinks(): Observable<ExclusionLink[]> {
     return this.handler(this.getSelectiveLinksHandler)
   }
 
+  /**
+   * Handler for creating regular exclusions section
+   * @return {Observable<ExclusionLink[]>}
+   */
   private getRegularLinksHandler(): Observable<ExclusionLink[]> {
     return new Observable((subscriber) => {
       let regularTransaction = this.db.transaction('regularModeLinks', 'readonly');
@@ -86,6 +118,10 @@ export class ExclusionDbService {
     })
   }
 
+  /**
+   * Handler for creating selective exclusions section
+   * @return {Observable<ExclusionLink[]>}
+   */
   private getSelectiveLinksHandler(): Observable<ExclusionLink[]> {
     return new Observable((subscriber) => {
       let selectiveTransaction = this.db.transaction('selectiveModeLinks', 'readonly');
@@ -104,10 +140,22 @@ export class ExclusionDbService {
     })
   }
 
+  /**
+   * Handler for add exclusion link by chosen mode
+   * @param {string} mode
+   * @param {ExclusionLink} link
+   * @return {Observable<ExclusionLink>}
+   */
   public addLink(mode: string, link: ExclusionLink): Observable<ExclusionLink> {
     return this.handler(() => this.addLinkHandler(mode, link));
   }
 
+  /**
+   * Adds exclusion link to needed section of data base by chosen mode
+   * @param {string} mode
+   * @param {ExclusionLink} link
+   * @return {Observable<ExclusionLink>}
+   */
   private addLinkHandler(mode: string, link: ExclusionLink): Observable<ExclusionLink> {
     return new Observable((subscriber) => {
       let transaction;
@@ -137,10 +185,22 @@ export class ExclusionDbService {
     });
   }
 
+  /**
+   * Handler for remove exclusion link by chosen mode
+   * @param {string} mode
+   * @param {string} linkName
+   * @return {Observable<any>}
+   */
   public removeLink(mode: string, linkName: string): Observable<any> {
     return this.handler(() => this.removeLinkHandler(mode, linkName));
   }
 
+  /**
+   * Removes exclusion link from needed section of data base by chosen mode
+   * @param {string} mode
+   * @param {string} linkName
+   * @return {Observable<any>}
+   */
   private removeLinkHandler(mode: string, linkName: string): Observable<any> {
     return new Observable((subscriber) => {
       let transaction;
@@ -171,11 +231,23 @@ export class ExclusionDbService {
     })
   }
 
-  public changeLink(mode: string, link: ExclusionLink) {
+  /**
+   * Handler for change exclusion link by chosen mode
+   * @param {string} mode
+   * @param {ExclusionLink} link
+   * @return {Observable<any>}
+   */
+  public changeLink(mode: string, link: ExclusionLink): Observable<any> {
     return this.handler(() => this.changeLinkHandler(mode, link));
   }
 
-  private changeLinkHandler(mode: string, link: ExclusionLink) {
+  /**
+   * Changes exclusion link in needed section of data base by chosen mode
+   * @param {string} mode
+   * @param {ExclusionLink} link
+   * @return {Observable<any>}
+   */
+  private changeLinkHandler(mode: string, link: ExclusionLink): Observable<any> {
     return new Observable((subscriber) => {
       let transaction;
       let links;
@@ -204,10 +276,20 @@ export class ExclusionDbService {
     })
   }
 
+  /**
+   * Handler for remove data base section by chosen mode
+   * @param {string} mode 
+   * @return {Observable<string>}
+   */
   public removeDB(mode: string): Observable<string> {
     return this.handler(() => this.removeDBHandler(mode));
   }
 
+  /**
+   * Removes needed data base section by chosen mode
+   * @param {string} mode
+   * @return {Observable<string>}
+   */
   private removeDBHandler(mode: string): Observable<string> {
     return new Observable((subscriber) => {
       let transaction;

7e49a02fcb7414aae2d25f9cdfc62f99e93e5579
MaxPalan <maxpalanwork@gmail.com>
2022-07-11 20:16:50 +0300

    vpn reducer code is in order

--- src/app/core/store/vpn/vpn.selector.ts
+++ src/app/core/store/vpn/vpn.selector.ts
@@ -1,6 +1,6 @@
-import { AppState } from '../app.reducer';
-import { createSelector } from '@ngrx/store';
-import { VPNState } from './vpn.reducer';
+import {AppState} from '../app.reducer';
+import {createSelector} from '@ngrx/store';
+import {VPNState} from './vpn.reducer';
 
 export const selectVPN = (state: AppState) => state.vpn;
 

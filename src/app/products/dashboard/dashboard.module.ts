2274156e305553922b9ba56f5610d24c8e04d39b
MaxPalan <maxpalanwork@gmail.com>
2022-07-15 14:31:22 +0300

    small cleaning

--- src/app/products/dashboard/dashboard.module.ts
+++ src/app/products/dashboard/dashboard.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 import {containers} from "./containers";
 import {components} from "./conponents";
 import {DashboardRoutingModule} from "./dashboard-routing.module";
 import {ReactiveFormsModule} from "@angular/forms";
-import { BytesPipe } from 'src/app/core/pipes/bites.pipe';
+import {BytesPipe} from 'src/app/core/pipes/bites.pipe';
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {RequestInterceptorService} from "../../core/services/app.interceptor";
 import {PassPopupComponent} from 'src/app/core/components/pass-popup/pass-popup.component';
